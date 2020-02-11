2d4cbbdab04ccec9dab108e29a667f8942f14f20
David Fernandez Amoros <david@issi.uned.es>
2020-02-11 19:47:06 +0100

    Refixed scripts

--- createVarOrderings.sh
+++ createVarOrderings.sh
@@ -1,9 +1,4 @@
 #!/bin/bash
-if [ x$KCONF = x ]
-  then
-    echo variable KCONF not set
-    exit -1;
-fi
 SYSTEMS="axtls  buildroot  busybox  coreboot  embtoolkit  fiasco  freetz  toybox  uClibc"
 for i in $SYSTEMS
 do
