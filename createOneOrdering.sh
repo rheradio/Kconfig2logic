706787f9f9e5f22fe6b1f5f3bc1466a430920fea
David Fernandez Amoros <david@issi.uned.es>
2020-02-11 20:41:00 +0100

    Fixed creatOneOrdering

--- createOneOrdering.sh
+++ createOneOrdering.sh
@@ -4,7 +4,7 @@ if [ "$1" = "buildroot" ]; then
     EXTRA=$EXTRA" BR2_EXTERNAL=support/dummy-external"
 fi
   printf "Processing %30s\n" $1
-  code/bin/Kconfig2Logic  $EXTRA systems/$1                 >$1.logfile1 2>&1 
+  ../../code/bin/Kconfig2Logic  $EXTRA ../../systems/$1                 >$1.logfile1 2>&1 
 
 
 
