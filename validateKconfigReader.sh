7f12065ef66568d7b2c3208a1d29867aa9acae49
David Fernandez Amoros <david@issi.uned.es>
2020-02-11 23:59:01 +0100

    Fixed 0 solutions

--- validateKconfigReader.sh
+++ validateKconfigReader.sh
@@ -3,6 +3,8 @@
 # $2 -> number of generated tests
 
 POSSIBILITIES="Config.in Config.src Kconfig config/Config.in config/Config.src src/Config.in src/Config.src src/Kconfig extra/Configs/Config.in"
+right=0
+wrong=0
 for ONE in $POSSIBILITIES; do
     if [ -f systems/$1/$ONE ]; then
         FILE=$ONE
@@ -23,17 +25,17 @@ for i in `seq $2`; do
     cat configuration ../../translations/KconfigReader/$1.exp >$1.$i.exp
     ../../code/bin/extractVariables $1.$i.exp >$1.$i.var
     if ! ../../code/bin/Logic2BDD $EXTRA -min-nodes 10000 -base $1.$i  -cudd -constraint-reorder  minspan  $1.$i.var $1.$i.exp  >>logfile 2>&1; then
-       cat logfile
-       exit
-    fi
-
-    solutions=`../../code/bin/counter $1.$i`
-    if [ x$solutions != x1 ]; then
-        echo " "The test failed, solutions=$solutions
-        let "wrong+=1"
-    else 
-        printf "\n"
-        let "right+=1"
+    echo " The test failed, solutions=0"
+    let "wrong+=1"
+    else
+      solutions=`../../code/bin/counter $1.$i`
+      if [ x$solutions != x1 ]; then
+          echo " "The test failed, solutions=$solutions
+          let "wrong+=1"
+      else 
+          printf "\n"
+          let "right+=1"
+      fi
     fi
     
 rm -f  positive negated logfile $1.$i.data $1.$i.reorder $1.$i.var $1.$i.exp $1.$i.dddmp 2>/dev/null
