fd17e475c846e0cd09102e894c74fec5b7673c53
David Fernandez Amoros <david@issi.uned.es>
2020-02-11 22:31:19 +0100

    Fixed cleanResults

--- cleanResults.sh
+++ cleanResults.sh
@@ -1,14 +1,9 @@
 #!/bin/bash 
-if [ x$KCONF = x ]
-  then
-    echo variable KCONF not set
-    exit -1;
-fi
 SYSTEMS="axtls  buildroot  busybox  coreboot  embtoolkit  fiasco  freetz  toybox  uClibc"
 for i in $SYSTEMS
 do
   echo Cleaning system $i
-  cd $KCONF/systems/$i
+  cd systems/$i
   rm configuration* *.reorder *data *.exp logfile* 2>/dev/null
+  cd ../..
 done
-wait
